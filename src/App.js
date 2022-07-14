aba6e46e5bc2e2787d6eb3717e135cb6bdbab5ce
roshanadh57 <roshanadh57@gmail.com>
2022-07-14 11:50:45 +0545

    simple crud

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//import { Switch } from "react-router-dom"
+import Navbar from "./components/Navbar";
+import Users from "./components/Users";
+import UserCreate from "./components/UserCreate";
+import UserUpdate from "./components/UserUpdate";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      {/* <Users /> */}
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route exact path="/create" element={<UserCreate />} />
+        <Route exact path="/update/:id" element={<UserUpdate />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
